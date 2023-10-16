41144a6affe44c52ad8ca3bdf22f56ac3bf381e4
Jose BM <jjbaxin@sice.com>
2023-10-15 19:43:56 -0600

    Animaciones de un contenedor

--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -1,13 +1,68 @@
 import 'package:flutter/material.dart';
+//? Importación para operaciones matemáticas
+import 'dart:math' show Random;
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
   
   static const name = 'animated_screen';
 
   const AnimatedScreen({super.key});
 
   @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+
+  double width = 50;
+  double height = 50;  
+  Color color = Colors.indigo;
+  double borderRadius = 10.0;
+
+
+  void changeShape() {
+    final random = Random();
+    
+    width = random.nextInt(300) + 120;
+    height = random.nextInt(200) + 120;
+    borderRadius = random.nextInt(100) + 2.5;
+    color = Color.fromRGBO(
+      random.nextInt(255), // red
+      random.nextInt(255), // green
+      random.nextInt(255), // blue
+      1 // opacity
+      );
+  
+    //? Actualiza el state
+    setState( (){});
+  }
+
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Animated Container'),
+      ),
+      
+      body: Center(
+        child: AnimatedContainer(
+          duration: const Duration( milliseconds: 400 ),
+          curve: Curves.elasticInOut, //? Animación en el cambio de tamaño 
+          width: width <= 0 ? 0 : width,
+          height: height <= 0 ? 0 : height,
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(borderRadius <= 0 ? 0 : borderRadius)
+          ),
+        ),    
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: changeShape,
+        child: const Icon( Icons.play_arrow_outlined ),
+      ),
+
+    );
   }
 }
